015c201bc7d4c04533f7e00834612fe95d0486bc
Aaron Ogle <atogle@gmail.com>
2013-03-19 18:58:50 -0400

    Merge changes from Stomping Ground into utils.js

--- src/utils.js
+++ src/utils.js
@@ -27,11 +27,11 @@ var Shareabouts = Shareabouts || {};
 
     isSupported: function(userAgent) {
       switch (userAgent.browser.name) {
-        case 'Chrome':
-        case 'Firefox':
-        case 'Safari':
+        case "Chrome":
+        case "Firefox":
+        case "Safari":
           return true;
-        case 'Microsoft Internet Explorer':
+        case "Microsoft Internet Explorer":
           var firstDot = userAgent.browser.version.indexOf('.'),
               major = parseInt(userAgent.browser.version.substr(0, firstDot), 10);
 
@@ -154,6 +154,67 @@ var Shareabouts = Shareabouts || {};
         }
       };
       return newHandler;
+    },
+
+    callWithRetries: function(func, retryCount, context) {
+      var args = Array.prototype.slice.call(arguments, 3),
+          options = _.last(args),
+          errorHandler = options.error,
+          retries = 0;
+
+      if (!options) {
+        options = {};
+        args.push(options);
+      }
+
+      options.error = function() {
+        if (retries < retryCount) {
+          retries++;
+          setTimeout(function() {
+            func.apply(context, args);
+          }, retries * 100);
+        } else {
+          if (errorHandler) {
+            errorHandler.apply(context, arguments);
+          }
+        }
+      };
+
+      func.apply(context, args);
+    },
+
+    // Cookies! Om nom nom
+    // Thanks ppk! http://www.quirksmode.org/js/cookies.html
+    cookies: {
+      save: function(name,value,days) {
+        var expires;
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime()+(days*24*60*60*1000));
+          expires = "; expires="+date.toGMTString();
+        }
+        else {
+          expires = "";
+        }
+        document.cookie = name+"="+value+expires+"; path=/";
+      },
+      get: function(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0) === ' ') {
+            c = c.substring(1,c.length);
+          }
+          if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length,c.length);
+          }
+        }
+        return null;
+      },
+      destroy: function(name) {
+        this.save(name,"",-1);
+      }
     }
   };
 })(Shareabouts, moment);

7764280dd628b1aa0a32748ee060300f6c3c1e92
Aaron Ogle <atogle@gmail.com>
2014-04-24 12:05:08 -0400

    Add simple place detail view

--- src/map.js
+++ src/map.js
@@ -30,14 +30,34 @@ var Shareabouts = Shareabouts || {};
     return null;
   };
 
+  var focusLayer = function(layer, styleRule) {
+    if (styleRule.focusIcon) {
+      layer.setIcon(L.icon(styleRule.focusIcon));
+    } else if (styleRule.focusStyle) {
+      layer.setStyle(styleRule.focusStyle);
+    }
+  };
+
+  var unfocusLayer = function(layer, styleRule) {
+    if (styleRule.icon) {
+      layer.setIcon(L.icon(styleRule.icon));
+    } else if (styleRule.style) {
+      layer.setStyle(styleRule.style);
+    }
+  };
+
   NS.Map = function(options) {
     var self = this,
         el = $(options.el).get(0),
+        $map = $('<div class="shareabouts-map"></div>'),
+        // TODO: should this be its own widget?
+        $panel = $('<div class="shareabouts-panel"></div>'),
         i, layerOptions;
 
-    this.options = options;
+    $map.appendTo(el);
+    $panel.appendTo(el);
 
-    this.map = L.map(el, options.map);
+    this.map = L.map($map.get(0), options.map);
     for (i = 0; i < options.layers.length; ++i) {
       layerOptions = options.layers[i];
       L.tileLayer(layerOptions.url, layerOptions).addTo(this.map);
@@ -63,14 +83,29 @@ var Shareabouts = Shareabouts || {};
       }
     }).addTo(this.map);
 
+    // Render the place detail template
+    this.geoJsonLayer.on('click', function(evt) {
+      var tpl = options.templates['place-detail'],
+          featureData = evt.layer.feature,
+          styleRule = getStyleRule(featureData.properties, options.placeStyles),
+          html = tpl(featureData);
 
+      if (this.focusedLayer) {
+        unfocusLayer(this.focusedLayer, styleRule);
+      }
+
+      this.focusedLayer = evt.layer;
+      focusLayer(this.focusedLayer, styleRule);
+
+      $panel.html(html);
+
+    });
 
     this.placeCollection.fetchAllPages({
       pageSuccess: function(collection, data) {
         self.geoJsonLayer.addData(data);
       }
     });
-
   };
 
 }(Shareabouts, jQuery, Shareabouts.Util.console));

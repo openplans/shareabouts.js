3f6851ce05008e077ef2b8490502e091d6958b6b
Aaron Ogle <atogle@gmail.com>
2014-06-03 17:13:58 -0400

    Only hide the add button when the place form is visible. - In other words, show it when we're looking at place details. - Hide the summary info window when showing the place form.

--- src/streetview.js
+++ src/streetview.js
@@ -476,6 +476,9 @@ var Shareabouts = Shareabouts || {};
         if (plusMarkerInfoWindow) {
           plusMarkerInfoWindow.open(self.panorama, plusMarker);
         }
+
+        // hide the summary, if visible
+        summaryWindow.close();
       }
     });
 

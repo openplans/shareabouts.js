f424a8167c86631ba5ae6ba91d353d5e2b04239a
Aaron Ogle <atogle@gmail.com>
2014-06-10 21:03:50 -0400

    Refactor PanelLayout.

--- src/map.js
+++ src/map.js
@@ -21,36 +21,6 @@ var Shareabouts = Shareabouts || {};
     }
   };
 
-  NS.PanelLayout = Backbone.View.extend({
-    events: {
-      'click .shareabouts-close-button': 'handleCloseClick'
-    },
-    initialize: function() {
-      this.$content = this.$('.shareabouts-panel-content');
-    },
-    showContent: function(view) {
-      if (this.currentView && this.currentView.onClose) {
-        this.currentView.onClose();
-      }
-
-      this.currentView = view;
-      this.$content.html(view.render().el);
-      this.$el.parent().addClass('panel-open');
-      this.$el.parent().trigger('showpanel');
-
-      if (this.currentView.onShow) {
-        this.currentView.onShow();
-      }
-    },
-    handleCloseClick: function() {
-      this.$el.parent().removeClass('panel-open');
-      this.$el.parent().trigger('closepanel');
-      if (this.currentView.onClose) {
-        this.currentView.onClose();
-      }
-    }
-  });
-
   NS.Map = function(options) {
     var self = this,
         modelIdToLayerId = {},
--- src/streetview.js
+++ src/streetview.js
@@ -13,37 +13,6 @@ var Shareabouts = Shareabouts || {};
     marker.setIcon(styleRule.icon);
   };
 
-  NS.PanelLayout = Backbone.View.extend({
-    events: {
-      'click .shareabouts-close-button': 'handleCloseClick'
-    },
-    initialize: function() {
-      this.$content = this.$('.shareabouts-panel-content');
-    },
-    showContent: function(view) {
-      if (this.currentView && this.currentView.onClose) {
-        this.currentView.onClose();
-      }
-
-      this.currentView = view;
-      this.$content.html(view.render().el);
-      this.$el.parent().addClass('panel-open');
-      this.$el.parent().trigger('showpanel');
-
-      if (this.currentView.onShow) {
-        this.currentView.onShow();
-      }
-    },
-    handleCloseClick: function(evt) {
-      evt.preventDefault();
-      this.$el.parent().removeClass('panel-open');
-      this.$el.parent().trigger('closepanel');
-      if (this.currentView.onClose) {
-        this.currentView.onClose();
-      }
-    }
-  });
-
   NS.PlaceSurveyItemView = Backbone.Marionette.ItemView.extend({
     tagName: 'li',
     initialize: function(options) { this.options = options; },

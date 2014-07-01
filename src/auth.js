0de85075c3a7f63f4e1e6ad41b0d106828c1ebf0
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-07-01 14:00:51 -0400

    Add auth methods for saving and loading sessions

--- src/auth.js
+++ src/auth.js
@@ -93,9 +93,64 @@ var Shareabouts = Shareabouts || {};
       if (self.authWindow && !self.authWindow.closed) {
         self.authWindow.close();
       }
+      self.requestCurrentUserSession({
+        complete: function() {
+          self.bindEvents();
+        }
+      });
+    };
 
-      $.ajax({
-        url: options.apiRoot + 'users/current',
+    // ========================================
+    // User sessions
+
+    this.saveUserSession = function(data) {
+      // Take the user data and save it in a cookie. If
+      // there is no user data, assume that we should
+      // clear the user session.
+
+      if (data) {
+        NS.Util.cookies.save('user-id', data.id);
+        NS.Util.cookies.save('user-name', data.name);
+        NS.Util.cookies.save('user-username', data.username);
+        NS.Util.cookies.save('user-avatar-url', data.avatar_url);
+      } else {
+        this.clearUserSession();
+      }
+    };
+
+    this.getUserSession = function() {
+      // Load the user session from the cookies. If no
+      // user session data is present, return undefined.
+
+      var id = NS.Util.cookies.get('user-id');
+      if (id) {
+        return {
+          'id': id,
+          'name': NS.Util.cookies.get('user-name'),
+          'username': NS.Util.cookies.get('user-username'),
+          'avatar_url': NS.Util.cookies.get('user-avatar-url')
+        };
+      }
+    };
+
+    this.clearUserSession = function() {
+      // Clear the user session cookies.
+
+      NS.Util.cookies.destroy('user-id');
+      NS.Util.cookies.destroy('user-name');
+      NS.Util.cookies.destroy('user-username');
+      NS.Util.cookies.destroy('user-avatar-url');
+    };
+
+    this.requestCurrentUserSession = function(options) {
+      // Fetch the current user data from the API.
+
+      options = options || {};
+      var ajaxSuccess = options.success,
+          ajaxError = options.error;
+
+      $.ajax(_.extend({}, options, {
+        url: self.options.apiRoot + 'users/current',
         xhrFields: {
           withCredentials: true
         },
@@ -107,16 +162,56 @@ var Shareabouts = Shareabouts || {};
           // user's data.
           self.isAuthenticated = !!userData;
           self.userData = userData;
+          self.saveUserSession(userData);
 
           $(self).trigger('authsuccess', [userData]);
+          if (ajaxSuccess) { ajaxSuccess.apply(this, arguments); }
         },
-        error: function(jqXhr) {
+        error: function() {
           $(self).trigger('autherror');
-        },
-        complete: function() {
-          self.bindEvents();
+          if (ajaxError) { ajaxError.apply(this, arguments); }
         }
-      });
+      }));
+    };
+
+    this.requestNewUserSession = function(username, password, options) {
+      // Log a user in using their username and password,
+      // and store their data in the user session.
+
+      options = options || {};
+      var self = this,
+          ajaxError = options.error,
+          ajaxComplete = options.complete;
+
+      // First, perform a request to log the user in.
+      $.ajax(_.extend({}, options, {
+        url: self.options.apiRoot + 'users/current',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          'username': username,
+          'password': password
+        }),
+        xhrFields: {withCredentials: true},
+
+        success: function() {
+          // This is going to be a CORS request, which doesn't allow redirects,
+          // so response will just be a string that is the URL for the user info.
+          // We now have to perform an additional request to get the actual user
+          // data.
+          self.requestCurrentUserSession(options);
+        },
+        error: function() {
+          $(self).trigger('autherror');
+          if (ajaxError) { ajaxError.apply(this, arguments); }
+        },
+        complete: function(jqXHR, status) {
+          // For a success case, we want to wait until we get the current
+          // user to run complete. For all other cases, run it immediately.
+          if (ajaxComplete && status === 'success') { return; }
+          ajaxComplete.apply(this, arguments);
+        }
+      }));
     };
   };
 }(Shareabouts, jQuery, Shareabouts.Util.console));

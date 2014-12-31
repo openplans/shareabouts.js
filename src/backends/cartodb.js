ef90f1a0bdf434d20bb16f00b55805cc90fe2c08
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-12-31 01:43:17 -0500

    An early CartoDB backend

---
+++ src/backends/cartodb.js
@@ -0,0 +1,53 @@
+/* global _, Backbone, jQuery */
+
+var Shareabouts = Shareabouts || {};
+
+(function(NS, $) {
+  'use strict';
+
+  NS.CartoDBBackend = function(options) {
+    /* Options
+     * -------
+     * - username
+     * - sqlurl
+     * - apikey
+     *
+     * Either username or sqlurl must be specified.
+     */
+    options = options || {};
+
+    this.username = options.username;
+    this.apikey = options.apikey;
+    this.sqlurl = options.sqlurl;
+
+    this.tables = _.defaults(options.tables || {}, {
+      places: 'places'
+    });
+  };
+
+  NS.CartoDBBackend.prototype = {
+    sync: function(method, model, options) {
+      // Interpret the request
+      if (model instanceof NS.PlaceCollection) {
+        options.url = this.getSQLURL();
+        options.type = 'GET';
+        _.extend(options.data, {
+          q: encodeURIComponent('SELECT * FROM ' + this.tables.places + ';'),
+          format: 'GeoJSON'
+        });
+      }
+
+      return Backbone.sync.apply(this, [method, model, options]);
+    },
+    getSQLURL: function() {
+      return this.sqlurl || ('http://' + this.username + '.cartodb.com/api/v2/sql/');
+    },
+    runSQL: function(sql, options) {
+      var url = this.getSQLURL() +
+          '?q=' + encodeURIComponent(sql) +
+          (this.apikey ? '&api_key=' + this.apikey : '') +
+          (options.geojson ? '&format=GeoJSON' : '');
+      return $.getJSON(url, options.success, options.error);
+    }
+  };
+}(Shareabouts, jQuery));
